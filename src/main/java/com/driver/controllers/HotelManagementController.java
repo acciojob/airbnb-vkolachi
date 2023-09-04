9cb1f20118280155fa7ab27303aa4f33c9e95a57
vkola <vkolachi@gmail.com>
2023-09-04 11:32:45 +0530

    test case

--- src/main/java/com/driver/controllers/HotelManagementController.java
+++ src/main/java/com/driver/controllers/HotelManagementController.java
@@ -1,9 +1,11 @@
 package com.driver.controllers;
 
+import com.driver.HotelManagementService;
 import com.driver.model.Booking;
 import com.driver.model.Facility;
 import com.driver.model.Hotel;
 import com.driver.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,6 +22,8 @@ import java.util.UUID;
 @RestController
 @RequestMapping("/hotel")
 public class HotelManagementController {
+    @Autowired
+    HotelManagementService hotelManagementService;
 
     @PostMapping("/add-hotel")
     public String addHotel(@RequestBody Hotel hotel){
@@ -30,7 +34,7 @@ public class HotelManagementController {
         //in all other cases return SUCCESS after successfully adding the hotel to the hotelDb.
 
 
-        return null;
+        return hotelManagementService.addHotel(hotel);
     }
 
     @PostMapping("/add-user")
@@ -38,8 +42,9 @@ public class HotelManagementController {
 
         //You need to add a User Object to the database
         //Assume that user will always be a valid user and return the aadharCardNo of the user
+        return hotelManagementService.addUser(user);
 
-       return null;
+
     }
 
     @GetMapping("/get-hotel-with-most-facilities")
@@ -49,7 +54,7 @@ public class HotelManagementController {
         //Incase there is a tie return the lexicographically smaller hotelName
         //Incase there is not even a single hotel with atleast 1 facility return "" (empty string)
 
-        return null;
+        return hotelManagementService.getHotelWithMostFacilities();
     }
 
     @PostMapping("/book-a-room")
@@ -62,14 +67,14 @@ public class HotelManagementController {
         //If there arent enough rooms available in the hotel that we are trying to book return -1 
         //in other case return total amount paid 
         
-        return 0;
+        return hotelManagementService.bookARoom(booking);
     }
     
     @GetMapping("/get-bookings-by-a-person/{aadharCard}")
     public int getBookings(@PathVariable("aadharCard")Integer aadharCard)
     {
         //In this function return the bookings done by a person 
-        return 0;
+        return hotelManagementService.getBookings(aadharCard);
     }
 
     @PutMapping("/update-facilities")
@@ -79,7 +84,7 @@ public class HotelManagementController {
         //If the hotel is already having that facility ignore that facility otherwise add that facility in the hotelDb
         //return the final updated List of facilities and also update that in your hotelDb
         //Note that newFacilities can also have duplicate facilities possible
-        return null;
+        return hotelManagementService.updateFacilities(newFacilities,hotelName);
     }
 
 }
